7c7e6d2718aa93d7a45eeef02cc3e8ee26fae99b
SWAone <abass.fadeil1100@gmail.com>
2023-06-01 19:46:54 +0300

    end

--- lib/view/timee.dart
+++ lib/view/timee.dart
@@ -21,60 +21,62 @@ class timee extends StatelessWidget {
               key: sc.formk,
               child: Directionality(
                 textDirection: TextDirection.rtl,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Divider(
-                      color: Colors.white,
-                    ),
-                    IconButton(
-                        onPressed: () {
-                          Get.back();
-                        },
-                        icon: Icon(
-                          Icons.arrow_forward,
-                          color: Colors.black,
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 1),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: ineed.custemTextForm(
-                          color: Colors.black,
-                          lable: 'اسم الخدمة ',
-                          onSaved: (p0) {
-                            sc.title = p0;
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Divider(
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            Get.back();
                           },
+                          icon: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: ineed.custemTextForm(
+                            color: Colors.black,
+                            lable: 'اسم الخدمة ',
+                            onSaved: (p0) {
+                              sc.title = p0;
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                    Center(
-                      child: DurationPicker(
-                        duration: sc.timepic,
-                        onChange: (val) {
-                          sc.time = val.inMinutes;
-                          print(val.inMinutes);
-                          sc.UpdateTimePic(val);
-                        },
-                        snapToMins: 5.0,
+                      Center(
+                        child: DurationPicker(
+                          duration: sc.timepic,
+                          onChange: (val) {
+                            sc.time = val.inMinutes;
+                            print(val.inMinutes);
+                            sc.UpdateTimePic(val);
+                          },
+                          snapToMins: 5.0,
+                        ),
                       ),
-                    ),
-                    Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: AppColor.mainColor,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: TextButton(
-                            onPressed: () {
-                              sc.addServes();
-                            },
-                            child:
-                                ineed.custmText(data: 'اضافة', isbold: true)),
-                      ),
-                    )
-                  ],
+                      Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: AppColor.mainColor,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: TextButton(
+                              onPressed: () {
+                                sc.addServes();
+                              },
+                              child:
+                                  ineed.custmText(data: 'اضافة', isbold: true)),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             );
